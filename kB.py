500c0f223c8c51bea433cd5f8a5287bd154bf3fe
Berj Chilingirian <berjc@mit.edu>
2015-07-19 05:10:11 -0400

    fix tabs

--- kB.py
+++ kB.py
@@ -1,9 +1,9 @@
 from optparse import OptionParser as op
 
 def init_parser():
-	parser = op()
-	parser.add_option('-i', '--input', dest='audio_dir', help='use audio pieces in SRC_DIRECTORY', metavar='SRC_DIRECTORY')
-	return parser.parse_args()
+  parser = op()
+  parser.add_option('-i', '--input', dest='audio_dir', help='use audio pieces in SRC_DIRECTORY', metavar='SRC_DIRECTORY')
+  return parser.parse_args()
 
 if __name__ == '__main__':
-	(options, args) = init_parser()
+  (options, args) = init_parser()
