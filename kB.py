283d8bd304f628552d0722913c975b4e3ee25f13
Berj Chilingirian <berjc@mit.edu>
2015-07-20 00:46:40 -0400

    add seed song option to command line parser

--- kB.py
+++ kB.py
@@ -3,6 +3,7 @@ from optparse import OptionParser as op
 def init_parser():
   parser = op()
   parser.add_option('-i', '--input', dest='audio_dir', help='use audio pieces in SRC_DIRECTORY', metavar='SRC_DIRECTORY')
+  parser.add_option('-s', '--seed', dest='seed', help='starts track list with SEED', metavar='SEED')
   return parser.parse_args()
 
 if __name__ == '__main__':
